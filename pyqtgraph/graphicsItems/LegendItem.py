7b8a3dc6b67d48f3477809ca4038e32a54ccbf32
Felix Jaeckel <45701052+fxjaeckel@users.noreply.github.com>
2019-05-02 13:59:18 -0500

    Fix alignment of LabelItems in legend (see https://github.com/pyqtgraph/pyqtgraph/issues/578)

--- pyqtgraph/graphicsItems/LegendItem.py
+++ pyqtgraph/graphicsItems/LegendItem.py
@@ -70,15 +70,17 @@ class LegendItem(GraphicsWidget, GraphicsWidgetAnchor):
         title           The title to display for this item. Simple HTML allowed.
         ==============  ========================================================
         """
-        label = LabelItem(name)
+        label = LabelItem(name, justify='left')
         if isinstance(item, ItemSample):
             sample = item
         else:
             sample = ItemSample(item)        
         row = self.layout.rowCount()
         self.items.append((sample, label))
+        self.layout.setRowAlignment(row, QtCore.Qt.AlignLeft) #added
         self.layout.addItem(sample, row, 0)
-        self.layout.addItem(label, row, 1)
+        self.layout.addItem(LabelItem(''), row, 1) #space
+        self.layout.addItem(label, row, 2)
         self.updateSize()
     
     def removeItem(self, item):
